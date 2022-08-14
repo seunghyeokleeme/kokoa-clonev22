75f73943a5f812e5be90631ec325d39fc18f7b81
devinan <devinan.hr@gmail.com>
2022-08-15 04:42:09 +0900

    Simplify app.listen in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World!!!");
+  res.send("Kakao Clone");
 });
 
-function handleListening() {
-  console.log("http://localhost:4000");
-}
-
-app.listen(4000, handleListening);
+app.listen(PORT, () => console.log("http://localhost:4000"));
