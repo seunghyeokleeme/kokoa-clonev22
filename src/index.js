0b20f996e3011c1186b4428f4672e6a84fb7ef6c
devinan <devinan.hr@gmail.com>
2022-08-15 06:18:51 +0900

    Add app.get() Route in index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,27 @@ app.use(logger);
 app.get("/", function (req, res) {
   res.send("Kakao Clone");
 });
+app.get("/join", function (req, res) {
+  res.send("Kakao Join");
+});
+app.get("/login", function (req, res) {
+  res.send("Kakao Login");
+});
+app.get("/rooms", function (req, res) {
+  res.send("Kakao Chat Room");
+});
+app.get("/views", function (req, res) {
+  res.send("Kakao Views list");
+});
+app.get("/shoppings", function (req, res) {
+  res.send("Kakao Shopping nav");
+});
+app.get("/mores", function (req, res) {
+  res.send("Kakao Mores nav");
+});
+app.get("/settings", function (req, res) {
+  res.send("Kakao Settings nav");
+});
 
 app.listen(PORT, () =>
   console.log(`🚀 Kokoa-clone app listening on port http://localhost:${PORT}`)
