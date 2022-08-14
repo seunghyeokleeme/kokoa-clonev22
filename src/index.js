d54cfeb3add9bb20e948149919381c2186a4c0b8
devinan <devinan.hr@gmail.com>
2022-08-15 04:45:57 +0900

    Add morgan for logger in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
 app.get("/", function (req, res) {
   res.send("Kakao Clone");
